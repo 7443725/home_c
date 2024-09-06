3ac570798a689b28391e1a248bb5c2b8b1fb858e
Dmitry <129031631+7443725@users.noreply.github.com>
2024-09-06 09:40:11 +0300

    Add files via upload

---
+++ home_work1/proba.c
@@ -0,0 +1,12 @@
+// Не относиться к домашней работе
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int x; 
+    cout << "Enter a number: "; 
+    cin >>x ;
+    cout << "You entered " << x << endl;
+    return 0;
+}
